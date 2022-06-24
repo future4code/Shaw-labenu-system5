91cf5123814f78cdae26e985ec25155601ca1b5f
Lis Ribeiro <lisribeiro197@gmail.com>
2022-06-24 13:04:10 -0300

    Formatacao de data

--- src/endpoints/createEstudante.ts
+++ src/endpoints/createEstudante.ts
@@ -5,10 +5,13 @@ import EstudantesDatabase from '../data/EstudantesDatabese'
 export default async function Estudante(req: Request, res: Response): Promise<void> {
     try {
         const { nome, email, data_nasc, turma_id} = req.body
-        const estudante = new EstudanteModel(nome, email, data_nasc,  turma_id )
+        const newDate = data_nasc.substr(0, 10).split('/').reverse().join('-')
+        const estudante = new EstudanteModel(nome, email, newDate, turma_id )
         const estudanteDB = new EstudantesDatabase()
         await estudanteDB.insertEstudante(estudante)
         res.status(201).send("Estudante Criado!")
+        console.log(data_nasc)
+        console.log(newDate)
         console.log(estudante)
 
     } catch (error: any) {
